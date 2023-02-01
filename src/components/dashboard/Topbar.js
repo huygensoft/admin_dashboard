3e1122f5428ae17e7e0304c6c877373bf218613a
Afolabi Esther <starafolab@gmail.com>
2023-02-02 00:12:45 +0100

    Add React

---
+++ src/components/dashboard/Topbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Topbar.scss';
+
+export default function Topbar() {
+  return (
+    <div className='top-bar'>
+        <div className='topbar-container'>
+            <div className='top-left'>
+                left
+            </div>
+            <div className='top-right'>
+                right
+            </div>
+        </div>
+
+
+    </div>
+  )
+}
