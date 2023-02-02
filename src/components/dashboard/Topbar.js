6afcf565194ec8be82abf9176d70c35802117235
Afolabi Esther <starafolab@gmail.com>
2023-02-02 01:05:52 +0100

    Add React#2

--- src/components/dashboard/Topbar.js
+++ src/components/dashboard/Topbar.js
@@ -1,16 +1,22 @@
 import React from 'react'
 import './Topbar.scss';
+import logo from './logo.svg';
 
 export default function Topbar() {
   return (
     <div className='top-bar'>
         <div className='topbar-container'>
             <div className='top-left'>
-                left
+                <img src={logo} alt='Booksrites logo' className='topbar-logo'/>
+                <span className='top-logo-tag'>Booksrite</span>
             </div>
-            <div className='top-right'>
-                right
+            <div className='top-right'>Dashboard </div>
+            <div>
+              <form>
+                <input type='search' id='search' placeholder='Search Dashboard'/>
+              </form>
             </div>
+            <div> Notify</div>
         </div>
 
 
